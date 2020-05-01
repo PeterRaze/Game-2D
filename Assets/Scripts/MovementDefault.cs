a57d955c432ef7aa9cca83a0b97e8c41f8c348d2
Pedro Neto <pedromunizbneto@gmail.com>
2020-05-01 20:13:05 -0300

    Scene First Sample

---
+++ Assets/Scripts/MovementDefault.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementDefault : MonoBehaviour
+{
+
+    private float speed = 5f, translation;
+    private bool jump = false;
+    private Rigidbody2D rigibody2D;
+
+    // Update is called once per frame
+    void Update()
+    {
+        translation = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        this.transform.Translate(translation, 0, 0);
+
+        if (jump)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                this.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 5f), ForceMode2D.Impulse);
+                jump = false;
+            }
+
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        jump = true;
+    }
+}
